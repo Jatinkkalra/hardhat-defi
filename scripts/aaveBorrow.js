49bd9bc727743944b9bed0805e270d6cd9bdc017
Jatinkkalra <jatinkkalra2@gmail.com>
2023-05-29 23:33:23 +0530

    course done

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -22,13 +22,34 @@ async function main() {
   await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0);
   console.log("Deposited");
 
-  // Borrowing steps now (How much we have in collateral, how much we have borrowed, how much we can borrow)
-  let { totalCollateralETH, totalDebtEth, availableBorrowsETH } = await getBorrowUserData(
-    lendingPool,
-    deployer
-  );
+  // Borrowing steps now
+  // How much we have in collateral, how much we have borrowed, how much we can borrow
+  let { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+    await getBorrowUserData(lendingPool, deployer);
 
   // availableBorrowsETH: What the conversion rate of DAI is?
+  const daiPrice = await getDaiPrice();
+  // How much we can borrow in DAI?
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber()); // 0.95 as we don't want to hit the maximum borrow limit
+  console.log(`You can borrow ${amountDaiToBorrow} DAI`);
+  // How much we can borrow in DAI? (in 18 decimal places)
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  );
+
+  // Borrowing
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // Source: https://etherscan.io/token/0x6b175474e89094c44da98b954eedeac495271d0f
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer);
+
+  // Running the user statistics function again
+  await getBorrowUserData(lendingPool, deployer);
+
+  // Repaying steps now
+  await repay(amountDaiToBorrowWei, daiTokenAddress, lendingPool, deployer);
+
+  // Running the user statistics function again
+  await getBorrowUserData(lendingPool, deployer);
 }
 
 // Lending pool needs to be fetched from Lending pool address provider. Lending pool address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
@@ -67,15 +88,47 @@ async function approveErc20(
 }
 
 async function getBorrowUserData(lendingPool, account) {
-  const { totalCollateralETH, totalDebtEth, availableBorrowsETH } =
+  const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
     await lendingPool.getUserAccountData(account);
   console.log(`You have ${totalCollateralETH} worth of ETH deposited`);
-  console.log(`You have ${totalDebtEth} worth of ETH borrowed`);
+  console.log(`You have ${totalDebtETH} worth of ETH borrowed`);
   console.log(`You can borrow ${availableBorrowsETH} worth of ETH`);
-  return { totalCollateralETH, totalDebtEth, availableBorrowsETH };
+  return { totalCollateralETH, totalDebtETH, availableBorrowsETH };
 } // Source: https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool#getuseraccountdata
 
-async function getDaiPrice(){}
+async function getDaiPrice() {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface", // abi
+    "0x773616E4d11A78F511299002da57A0a94577F1f4" // DAI/ETH contract address: https://docs.chain.link/data-feeds/price-feeds/addresses
+    // No need assign signer as we will only be reading the price
+  );
+
+  const price = (await daiEthPriceFeed.latestRoundData())[1]; // function from AggregatorV3Interface.sol
+  console.log(`The DAI/ETH price is ${price.toString()} USD`);
+  return price;
+}
+
+async function borrowDai(daiAddress, lendingPool, amountDaiToBorrow, account) {
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrow,
+    1,
+    0,
+    account
+  ); // Source: https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool#borrow
+  await borrowTx.wait(1);
+  console.log(`You have borrowed!`);
+}
+
+async function repay(amount, daiAddress, lendingPool, account) {
+  // daiAddress is the address we are going to repay to
+  // To repay, we will first have to approve sending our DAI back to Aave
+  await approveErc20(daiAddress, lendingPool.address, amount, account);
+  // Repaying now
+  const repayTx = await lendingPool.repay(daiAddress, amount, 1, account); // Source: https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool#repay
+  await repayTx.wait(1);
+  console.log("Repaid!");
+}
 
 // boiler code for scripts
 main()
